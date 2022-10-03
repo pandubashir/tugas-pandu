e45802551e0d29753e0203b2b26ff18ba235ce2f
pandu <pandubashir@gmail.com>
2022-10-03 23:02:47 +0700

    fisrt commit

---
+++ untilted-2.py
@@ -0,0 +1,9 @@
+nama_mhs = str(input("Masukan Nama Anda = "))
+nim_mhs = str(input("Masukan NIM Anda = "))
+
+
+print("")
+print("")
+tampilInfoMhs = "Detail dari Informasi Anda..\n\nNama Anda Adalah : "+nama_mhs+"\nNim Anda Adalah :"
+print(tampilInfoMhs)
+print("Nama Anda adalah {} dengan nim {} bernama ".format(nama_mhs, nim_mhs,))
---
+++ untilted-3.py
@@ -0,0 +1,8 @@
+a = 13
+b = 2
+jumlah = a + b #
+apel = 7
+jeruk = 9
+buah = apel + jeruk #
+print("hasil penjumlahan", jumlah)
+print("jumlah buah apel dan jeruk adalah", buah)
